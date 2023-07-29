336866a50439e2f6d227e84f5819ea14fe99cb0c
dukk66 <132862536+dukk66@users.noreply.github.com>
2023-07-29 21:19:33 +0700

    Add files via upload
    add layout subject

--- index.blade.php
+++ index.blade.php
@@ -1,38 +1,41 @@
 @extends('layout.base')
-@section('page_title', 'List Student')
+@section('page_title', 'List of grades for the subject: '.$rec->name)
 @section('slot')
 <div class="card">
     <div class="card-body px-0 pb-2">
         <div class="table-responsive p-0">
             <table class="table align-items-center mb-0">
                 <thead>
-
                     <tr>
-                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Full Name</th>
-                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Account Name</th>
-                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Date Of Birth</th>
+                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Full Name Student</th>
                         <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Student ID</th>
-                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Class</th>
+                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">component point 1</th>
+                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">component point 2</th>
+                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">point process</th>
+                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">final point</th>
+                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">summary point</th>
                         <th class="text-secondary opacity-7"></th>
                     </tr>
                 </thead>
                 <tbody>
                     @forelse($rows as $row)
                     <tr>
-                        <td class="text-xs">{{$row->name}}</td>
-                        <td class="text-xs">{{$row->username}}</td>
-                        <td class="text-xs">{{date('d/m/Y', strtotime($row->profile->dob))}}</td>
-                        <td class="text-xs">{{$row->profile->code}}</td>
-                        <td class="text-xs">{{$row->profile->class->name}}</td>
+                        <td class="text-xs">{{$row->student->user->name}}</td>
+                        <td class="text-xs">{{$row->student->code}}</td>
+                        <td class="text-xs">{{$row->tp1}}</td>
+                        <td class="text-xs">{{$row->tp2}}</td>
+                        <td class="text-xs">{{$row->qt}}</td>
+                        <td class="text-xs">{{$row->ck}}</td>
+                        <td class="text-xs">{{$row->tk}}</td>
                         <td class="align-middle">
                             <a class="text-secondary font-weight-bold text-xs"
-                                href="{{route('students.edit', ['id' => $row->id])}}">Change</a> |
+                                href="{{route('scores.edit', ['id' => $row->id])}}">Change</a> | 
                             <a class="text-secondary font-weight-bold text-xs"
-                                href="{{route('students.delete', ['id' => $row->id])}}">Delete</a>
+                                href="{{route('scores.delete', ['id' => $row->id])}}">Delete</a>
                         </td>
                     </tr>
                     @empty
-                    <tr><td class="align-middle text-secondary font-weight-bold text-xs">no data discovery</td></tr>
+                    <tr><td class="align-middle text-secondary font-weight-bold text-xs">No data</td></tr>
                     @endforelse
                 </tbody>
             </table>
