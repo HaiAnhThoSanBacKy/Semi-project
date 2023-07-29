92ddbe16370b9c68495a06efba343e025f980977
dukk66 <132862536+dukk66@users.noreply.github.com>
2023-07-29 21:08:11 +0700

    Add files via upload
    remove input

--- index.blade.php
+++ index.blade.php
@@ -1,15 +1,18 @@
 @extends('layout.base')
-@section('page_title', 'Subjects')
+@section('page_title', 'List Student')
 @section('slot')
 <div class="card">
     <div class="card-body px-0 pb-2">
         <div class="table-responsive p-0">
             <table class="table align-items-center mb-0">
                 <thead>
+
                     <tr>
-                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Subject name</th>
-                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Subject ID</th>
-                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Semester</th>
+                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Full Name</th>
+                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Account Name</th>
+                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Date Of Birth</th>
+                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Student ID</th>
+                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Class</th>
                         <th class="text-secondary opacity-7"></th>
                     </tr>
                 </thead>
@@ -17,19 +20,19 @@
                     @forelse($rows as $row)
                     <tr>
                         <td class="text-xs">{{$row->name}}</td>
-                        <td class="text-xs">{{$row->code}}</td>
-                        <td class="text-xs">{{$row->semester}}</td>
+                        <td class="text-xs">{{$row->username}}</td>
+                        <td class="text-xs">{{date('d/m/Y', strtotime($row->profile->dob))}}</td>
+                        <td class="text-xs">{{$row->profile->code}}</td>
+                        <td class="text-xs">{{$row->profile->class->name}}</td>
                         <td class="align-middle">
-                            @if(in_array(auth()->user()->role, ['teacher']))
                             <a class="text-secondary font-weight-bold text-xs"
-                                href="{{route('subjects.edit', ['id' => $row->id])}}">Change</a> |
+                                href="{{route('students.edit', ['id' => $row->id])}}">Change</a> |
                             <a class="text-secondary font-weight-bold text-xs"
-                                href="{{route('subjects.delete', ['id' => $row->id])}}">Delete</a>
-                            @endif
+                                href="{{route('students.delete', ['id' => $row->id])}}">Delete</a>
                         </td>
                     </tr>
                     @empty
-                    <tr><td class="align-middle text-secondary font-weight-bold text-xs">no data discover</td></tr>
+                    <tr><td class="align-middle text-secondary font-weight-bold text-xs">no data discovery</td></tr>
                     @endforelse
                 </tbody>
             </table>
